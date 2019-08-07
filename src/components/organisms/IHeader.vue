dc463669a85be602646f67e77e4bab4c6811ee85
dinhlinhx <50461902+dinhlinhx@users.noreply.github.com>
2019-08-07 09:44:50 +0700

    Update IHeader.vue

--- src/components/organisms/IHeader.vue
+++ src/components/organisms/IHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="light-blue darken-4">
     <div class="nav-wrapper">
-      <a href="#" class="brand-logo pl-2 pr-2">Logo</a>
+      <a href="#" class="brand-logo pl-2 pr-2">NAMING CONVENTION CONVERTER</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="https://github.com/dinhlinhx/case-converter" target="_blank">Github</a></li>
       </ul>
